2af38da7dfbbc4036c2e79c51833d2f7d141ab79
Ton Blom <32536211+BlomTbl@users.noreply.github.com>
2026-03-19 10:27:15 +0100

    Update __init__.py

--- nzingaflow/__init__.py
+++ nzingaflow/__init__.py
@@ -56,6 +56,7 @@ from .lta         import (
     wall_first_order_multi,
     compute_wall_k,
     advect,
+    exit_detect,
     node_mixing_multi,
     tank_step_implicit,
 )
@@ -64,7 +65,7 @@ from .geochemistry import GeochemSolver, SpeciesMap, chlorine_decay_geochem, ful
 from .lta          import warmup_numba, USE_NUMBA, combined_decay_multi, build_combined_exp
 from .merging      import warmup_numba_merging
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__  = "NzingaFlow"
 
 __all__ = [
@@ -89,6 +90,7 @@ __all__ = [
     "wall_first_order_multi",
     "compute_wall_k",
     "advect",
+    "exit_detect",
     "node_mixing_multi",
     "tank_step_implicit",
     "merge_segments",
